9b914676af14cf748d8461755f446eaa962f78a9
JonahFarc <jonahfarc@gmail.com>
2017-11-13 02:08:27 -0600

    Added all logic. Revised diagram for faster output. Validation on input. All logic should be functioning correctly. Animations still need to be added.

--- src/ALU.java
+++ src/ALU.java
@@ -3,14 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.util.Arrays;
 import javax.swing.text.*;
 import javax.swing.text.DocumentFilter.*;
+import java.util.concurrent.TimeUnit;
 /**
  *
  * @author Chawp
  */
 public class ALU extends javax.swing.JFrame {
-
+    private String[] mux1 = new String[3];
+    private String[] mux2 = new String[10];
+    private String mux1out = "";
+    private String mux2out = "";
     /**
      * Creates new form ALU
      */
@@ -64,7 +69,7 @@ public class ALU extends javax.swing.JFrame {
         loadLabel = new javax.swing.JLabel();
         nothingLabel = new javax.swing.JLabel();
         multLabel = new javax.swing.JLabel();
-        resetLabel1 = new javax.swing.JLabel();
+        resetLabel = new javax.swing.JLabel();
         offLabel = new javax.swing.JLabel();
         clockLabel2 = new javax.swing.JLabel();
         clockLabel = new javax.swing.JLabel();
@@ -72,6 +77,18 @@ public class ALU extends javax.swing.JFrame {
         FF1output = new javax.swing.JTextField();
         FF2Label = new javax.swing.JLabel();
         FF2Output = new javax.swing.JTextField();
+        addOutLabel = new javax.swing.JLabel();
+        resetOutLabel = new javax.swing.JLabel();
+        subOutLabel = new javax.swing.JLabel();
+        andOutLabel = new javax.swing.JLabel();
+        orOutLabel = new javax.swing.JLabel();
+        xorOutLabel = new javax.swing.JLabel();
+        notOutLabel = new javax.swing.JLabel();
+        loadOutLabel = new javax.swing.JLabel();
+        nothingOutLabel = new javax.swing.JLabel();
+        multOutLabel = new javax.swing.JLabel();
+        mux1OutLabel = new javax.swing.JLabel();
+        mux2OutLabel = new javax.swing.JLabel();
         BackgroundImage = new javax.swing.JLabel();
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -166,7 +183,7 @@ public class ALU extends javax.swing.JFrame {
         jScrollPane2.setViewportView(output);
 
         getContentPane().add(jScrollPane2);
-        jScrollPane2.setBounds(950, 160, 137, 33);
+        jScrollPane2.setBounds(930, 300, 137, 33);
 
         inputLabel.setText("Input");
         getContentPane().add(inputLabel);
@@ -174,19 +191,24 @@ public class ALU extends javax.swing.JFrame {
 
         outputLabel.setText("Output");
         getContentPane().add(outputLabel);
-        outputLabel.setBounds(960, 140, 40, 14);
+        outputLabel.setBounds(940, 280, 40, 14);
 
-        goButton.setText("I'M READY");
+        goButton.setText("Clock Tick");
+        goButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                goButtonActionPerformed(evt);
+            }
+        });
         getContentPane().add(goButton);
         goButton.setBounds(40, 230, 90, 23);
 
         addLabel.setText("ADD");
         getContentPane().add(addLabel);
-        addLabel.setBounds(330, 150, 70, 20);
+        addLabel.setBounds(270, 150, 70, 20);
 
         subLabel.setText("SUB");
         getContentPane().add(subLabel);
-        subLabel.setBounds(330, 190, 70, 20);
+        subLabel.setBounds(270, 190, 70, 20);
 
         onLabel.setText("1");
         getContentPane().add(onLabel);
@@ -194,35 +216,35 @@ public class ALU extends javax.swing.JFrame {
 
         andLabel.setText("AND");
         getContentPane().add(andLabel);
-        andLabel.setBounds(330, 220, 70, 30);
+        andLabel.setBounds(270, 220, 70, 30);
 
         orLabel.setText("OR");
         getContentPane().add(orLabel);
-        orLabel.setBounds(330, 260, 70, 20);
+        orLabel.setBounds(270, 260, 70, 20);
 
         xorLabel.setText("XOR");
         getContentPane().add(xorLabel);
-        xorLabel.setBounds(330, 300, 70, 20);
+        xorLabel.setBounds(270, 300, 70, 20);
 
         notLabel.setText("NOT");
         getContentPane().add(notLabel);
-        notLabel.setBounds(330, 330, 70, 30);
+        notLabel.setBounds(270, 330, 70, 30);
 
         loadLabel.setText("LOAD");
         getContentPane().add(loadLabel);
-        loadLabel.setBounds(330, 370, 70, 20);
+        loadLabel.setBounds(270, 370, 70, 20);
 
         nothingLabel.setText("Do Nothing");
         getContentPane().add(nothingLabel);
-        nothingLabel.setBounds(320, 400, 90, 40);
+        nothingLabel.setBounds(250, 400, 90, 40);
 
         multLabel.setText("MULT");
         getContentPane().add(multLabel);
-        multLabel.setBounds(330, 110, 70, 30);
+        multLabel.setBounds(270, 110, 70, 30);
 
-        resetLabel1.setText("0");
-        getContentPane().add(resetLabel1);
-        resetLabel1.setBounds(340, 80, 70, 20);
+        resetLabel.setText("RESET");
+        getContentPane().add(resetLabel);
+        resetLabel.setBounds(260, 80, 70, 20);
 
         offLabel.setText("0");
         getContentPane().add(offLabel);
@@ -241,7 +263,7 @@ public class ALU extends javax.swing.JFrame {
         FF1Label.setBounds(300, 760, 30, 20);
 
         FF1output.setEditable(false);
-        FF1output.setText("XX");
+        FF1output.setText("X");
         getContentPane().add(FF1output);
         FF1output.setBounds(270, 800, 80, 20);
 
@@ -250,11 +272,59 @@ public class ALU extends javax.swing.JFrame {
         FF2Label.setBounds(650, 150, 34, 14);
 
         FF2Output.setEditable(false);
-        FF2Output.setText("XX");
+        FF2Output.setText("XXXXXXXX");
         getContentPane().add(FF2Output);
         FF2Output.setBounds(620, 190, 80, 20);
 
-        BackgroundImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/ALU temp.png"))); // NOI18N
+        addOutLabel.setText("00000000");
+        getContentPane().add(addOutLabel);
+        addOutLabel.setBounds(360, 150, 60, 14);
+
+        resetOutLabel.setText("00000000");
+        getContentPane().add(resetOutLabel);
+        resetOutLabel.setBounds(360, 70, 60, 14);
+
+        subOutLabel.setText("00000000");
+        getContentPane().add(subOutLabel);
+        subOutLabel.setBounds(360, 180, 60, 14);
+
+        andOutLabel.setText("00000000");
+        getContentPane().add(andOutLabel);
+        andOutLabel.setBounds(360, 220, 60, 14);
+
+        orOutLabel.setText("00000000");
+        getContentPane().add(orOutLabel);
+        orOutLabel.setBounds(360, 250, 60, 14);
+
+        xorOutLabel.setText("00000000");
+        getContentPane().add(xorOutLabel);
+        xorOutLabel.setBounds(360, 290, 60, 14);
+
+        notOutLabel.setText("00000000");
+        getContentPane().add(notOutLabel);
+        notOutLabel.setBounds(360, 330, 60, 14);
+
+        loadOutLabel.setText("00000000");
+        getContentPane().add(loadOutLabel);
+        loadOutLabel.setBounds(360, 360, 60, 14);
+
+        nothingOutLabel.setText("00000000");
+        getContentPane().add(nothingOutLabel);
+        nothingOutLabel.setBounds(360, 400, 60, 14);
+
+        multOutLabel.setText("00000000");
+        getContentPane().add(multOutLabel);
+        multOutLabel.setBounds(360, 110, 60, 14);
+
+        mux1OutLabel.setText("0");
+        getContentPane().add(mux1OutLabel);
+        mux1OutLabel.setBounds(210, 790, 40, 14);
+
+        mux2OutLabel.setText("00000000");
+        getContentPane().add(mux2OutLabel);
+        mux2OutLabel.setBounds(530, 180, 60, 14);
+
+        BackgroundImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resources/ALU.png"))); // NOI18N
         getContentPane().add(BackgroundImage);
         BackgroundImage.setBounds(0, 0, 1145, 1000);
 
@@ -277,6 +347,190 @@ public class ALU extends javax.swing.JFrame {
             resetSet.setSelected(false);
     }//GEN-LAST:event_reset2SetActionPerformed
 
+    private void goButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_goButtonActionPerformed
+        //formats input value and sets to 0 if empty
+        int in;
+        in = input.getText().length() == 0? 0 : Integer.parseInt(input.getText(),2);
+        input.setText(format(in));
+        
+        //Order of things to do: feed values from flip flops into muxs, calculate mux values, select proper mux values, store in flip flop, output vals from mux
+        setMux1Values(FF1output.getText());
+        setMux2Values(FF2Output.getText(), input.getText());
+        mux1select();
+        mux2select();
+        FF1output.setText(mux1out);
+        FF2Output.setText(mux2out);
+        output.setText(mux2out);
+    }//GEN-LAST:event_goButtonActionPerformed
+    //calculates all the values to be output from mux1
+    private void setMux1Values(String FF)
+    {
+        System.out.println("setting mux1 with a flip flop val of: " + FF);
+        mux1[0] = FF;
+        mux1[1] = "1";
+        mux1[2] = "0";
+        System.out.println(Arrays.toString(mux1));
+    }
+    //calculates all the values to be output from mux2
+    private void setMux2Values(String FF, String Input)
+    {
+        
+        System.out.printf("setting mux1 with vals of: %s : %s\n",FF,Input);
+        //calculate values for each output
+        multOutLabel.setText(mult(FF, Input));
+        addOutLabel .setText(add (FF, Input));
+        subOutLabel .setText(sub (FF, Input));
+        andOutLabel .setText(and (FF, Input));
+        orOutLabel  .setText(or  (FF, Input));
+        xorOutLabel .setText(xor (FF, Input));
+        notOutLabel .setText(not (FF));
+        loadOutLabel.setText(load(    Input));
+        
+        nothingOutLabel.setText(FF);
+        
+        //assign values into mux
+        mux2[0] = resetOutLabel   .getText();
+        mux2[1] = multOutLabel    .getText();
+        mux2[2] = addOutLabel     .getText();
+        mux2[3] = subOutLabel     .getText();
+        mux2[4] = andOutLabel     .getText();
+        mux2[5] = orOutLabel      .getText();
+        mux2[6] = xorOutLabel     .getText();
+        mux2[7] = notOutLabel     .getText();
+        mux2[8] = loadOutLabel    .getText();
+        mux2[9] = nothingOutLabel .getText();
+        System.out.println(Arrays.toString(mux2));
+    }
+    //solves for the output of Mux 1
+    private void mux1select()//, boolean rst, boolean mult, boolean add, boolean sub, boolean and, boolean or, boolean xor, boolean not, boolean load)
+    {
+        if(reset2Set.isSelected() || onSet.isSelected())
+            mux1out = mux1[1];
+        else if(offSet.isSelected())
+            mux1out = mux1[2];
+        else
+            mux1out = mux1[0];
+        System.out.println("mux1 output is: " + mux1out);
+        mux1OutLabel.setText(mux1out);
+    }
+    //solves for the output of Mux 2
+    private void mux2select()
+    {
+        if(mux1out.equals("1"))
+        {
+            if(multSet.isSelected())
+                mux2out = mux2[1];
+            else if(addSet.isSelected())
+                mux2out = mux2[2];
+            else if(subSet.isSelected())
+                mux2out = mux2[3];
+            else if(andSet.isSelected())
+                mux2out = mux2[4];
+            else if(orSet.isSelected())
+                mux2out = mux2[5];
+            else if(xorSet.isSelected())
+                mux2out = mux2[6];
+            else if(notSet.isSelected())
+                mux2out = mux2[7];
+            else if(loadSet.isSelected())
+                mux2out = mux2[8];
+            else
+                mux2out = mux2[9];
+        }
+        else
+            mux2out = mux2[9];
+        if(resetSet.isSelected())
+            mux2out = mux2[0];
+        System.out.println("mux2 output is: " + mux2out);
+        mux2OutLabel.setText(mux2out);
+    }
+    //multiplies two binary values and formats the result
+    private String mult(String FF, String Input)
+    {
+       if(FF.equals("XXXXXXXX"))
+            return "XXXXXXXX";
+        int ffval = Integer.parseInt(FF, 2);
+        int inval = Integer.parseInt(Input, 2);
+        int product = ffval*inval;
+        return format(product); 
+    }
+    
+    //adds the two binary values together and formats the result
+    private String add(String FF, String Input)
+    {
+        if(FF.equals("XXXXXXXX"))
+            return "XXXXXXXX";
+        int ffval = Integer.parseInt(FF, 2);
+        int inval = Integer.parseInt(Input, 2);
+        int sum = ffval+inval;
+        return format(sum);
+    }
+    //subtracts the two binary values from each other and formats the result
+    private String sub(String FF, String Input)
+    {
+        if(FF.equals("XXXXXXXX"))
+            return "XXXXXXXX";
+        int ffval = Integer.parseInt(FF, 2);
+        int inval = Integer.parseInt(Input, 2);
+        int sum = ffval-inval;
+        return format(sum);
+    }
+    //ands the two binary values together and formats the result
+    private String and(String FF, String Input)
+    {
+        if(FF.equals("XXXXXXXX"))
+            return "XXXXXXXX";
+        int ffval = Integer.parseInt(FF, 2);
+        int inval = Integer.parseInt(Input, 2);
+        int and = ffval&inval;
+        return format(and);
+    }
+    //ors the two binary values together and formats the result
+    private String or(String FF, String Input)
+    {
+        if(FF.equals("XXXXXXXX"))
+            return "XXXXXXXX";
+        int ffval = Integer.parseInt(FF, 2);
+        int inval = Integer.parseInt(Input, 2);
+        int or = ffval|inval;
+        return format(or);
+    }
+    //xors the two binary values together and formats the result
+    private String xor(String FF, String Input)
+    {
+        if(FF.equals("XXXXXXXX"))
+            return "XXXXXXXX";
+        int ffval = Integer.parseInt(FF, 2);
+        int inval = Integer.parseInt(Input, 2);
+        int xor = ffval^inval;
+        return format(xor);
+    }
+    //nots the binary value and formats the result
+    private String not(String FF)
+    {
+        if(FF.equals("XXXXXXXX"))
+            return "XXXXXXXX";
+        int ffval = Integer.parseInt(FF, 2);
+        int not = ~ffval;
+        return format(not);
+    }
+    //loads the binary value and formats the result
+    private String load(String Input)
+    {
+        if(Input.length() == 0)
+            return "00000000";
+        int inval = Integer.parseInt(Input, 2);
+        return format(inval);
+    }
+    //formats a number into an 8bit binary number, truncated if it overflows
+    private String format(int num)
+    {
+        //convert the number to a minimum of 8 characters in bits
+        String output = String.format("%8s", Integer.toBinaryString(num)).replace(' ', '0');
+        //truncates number to last 8 bits
+        output = output.substring(output.length()-8, output.length());
+        return output;
+    }
     /**
      * @param args the command line arguments
      */
@@ -312,53 +566,9 @@ public class ALU extends javax.swing.JFrame {
         });
         
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel BackgroundImage;
-    private javax.swing.JLabel FF1Label;
-    private javax.swing.JTextField FF1output;
-    private javax.swing.JLabel FF2Label;
-    private javax.swing.JTextField FF2Output;
-    private javax.swing.JLabel addLabel;
-    private javax.swing.JCheckBox addSet;
-    private javax.swing.JLabel andLabel;
-    private javax.swing.JCheckBox andSet;
-    private javax.swing.JLabel clockLabel;
-    private javax.swing.JLabel clockLabel2;
-    private javax.swing.JButton goButton;
-    private javax.swing.JTextPane input;
-    private javax.swing.JLabel inputLabel;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JLabel loadLabel;
-    private javax.swing.JCheckBox loadSet;
-    private javax.swing.JLabel multLabel;
-    private javax.swing.JCheckBox multSet;
-    private javax.swing.JLabel notLabel;
-    private javax.swing.JCheckBox notSet;
-    private javax.swing.JLabel nothingLabel;
-    private javax.swing.JLabel offLabel;
-    private javax.swing.JCheckBox offSet;
-    private javax.swing.JLabel onLabel;
-    private javax.swing.JCheckBox onSet;
-    private javax.swing.ButtonGroup operation;
-    private javax.swing.JLabel orLabel;
-    private javax.swing.JCheckBox orSet;
-    private javax.swing.JTextPane output;
-    private javax.swing.JLabel outputLabel;
-    private javax.swing.JCheckBox reset2Set;
-    private javax.swing.JLabel resetLabel1;
-    private javax.swing.JCheckBox resetSet;
-    private javax.swing.JLabel subLabel;
-    private javax.swing.JCheckBox subSet;
-    private javax.swing.JLabel xorLabel;
-    private javax.swing.JCheckBox xorSet;
-    // End of variables declaration//GEN-END:variables
     class MyDocumentFilter extends javax.swing.text.DocumentFilter
     {
-        public void replace(DocumentFilter.FilterBypass fb, int offset, int length, String text,
-        AttributeSet attrs) throws BadLocationException 
+        public void replace(DocumentFilter.FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException 
         {
             System.out.println("replace: " + text);
             if(text.length() == 1)
@@ -393,4 +603,59 @@ public class ALU extends javax.swing.JFrame {
             }
         }
     }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BackgroundImage;
+    private javax.swing.JLabel FF1Label;
+    private javax.swing.JTextField FF1output;
+    private javax.swing.JLabel FF2Label;
+    private javax.swing.JTextField FF2Output;
+    private javax.swing.JLabel addLabel;
+    private javax.swing.JLabel addOutLabel;
+    private javax.swing.JCheckBox addSet;
+    private javax.swing.JLabel andLabel;
+    private javax.swing.JLabel andOutLabel;
+    private javax.swing.JCheckBox andSet;
+    private javax.swing.JLabel clockLabel;
+    private javax.swing.JLabel clockLabel2;
+    private javax.swing.JButton goButton;
+    private javax.swing.JTextPane input;
+    private javax.swing.JLabel inputLabel;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel loadLabel;
+    private javax.swing.JLabel loadOutLabel;
+    private javax.swing.JCheckBox loadSet;
+    private javax.swing.JLabel multLabel;
+    private javax.swing.JLabel multOutLabel;
+    private javax.swing.JCheckBox multSet;
+    private javax.swing.JLabel mux1OutLabel;
+    private javax.swing.JLabel mux2OutLabel;
+    private javax.swing.JLabel notLabel;
+    private javax.swing.JLabel notOutLabel;
+    private javax.swing.JCheckBox notSet;
+    private javax.swing.JLabel nothingLabel;
+    private javax.swing.JLabel nothingOutLabel;
+    private javax.swing.JLabel offLabel;
+    private javax.swing.JCheckBox offSet;
+    private javax.swing.JLabel onLabel;
+    private javax.swing.JCheckBox onSet;
+    private javax.swing.ButtonGroup operation;
+    private javax.swing.JLabel orLabel;
+    private javax.swing.JLabel orOutLabel;
+    private javax.swing.JCheckBox orSet;
+    private javax.swing.JTextPane output;
+    private javax.swing.JLabel outputLabel;
+    private javax.swing.JCheckBox reset2Set;
+    private javax.swing.JLabel resetLabel;
+    private javax.swing.JLabel resetOutLabel;
+    private javax.swing.JCheckBox resetSet;
+    private javax.swing.JLabel subLabel;
+    private javax.swing.JLabel subOutLabel;
+    private javax.swing.JCheckBox subSet;
+    private javax.swing.JLabel xorLabel;
+    private javax.swing.JLabel xorOutLabel;
+    private javax.swing.JCheckBox xorSet;
+    // End of variables declaration//GEN-END:variables
+    
 }
